2f24adc5565a92a1dd8f27023274977f7c3bc2c9
Tasneem Mahmud <tmahmud@spartaglobal.com>
2018-08-06 18:38:17 +0100

    created a calculator using html and css

---
+++ index.js
@@ -0,0 +1,7 @@
+// creating functions on each button
+var numbers = document.getElementsByClassName("number");
+var operators = document.getElementsByClassName("operator");
+var clear = document.getElementsByClassName("clear");
+var equals = document.getElementsByClassName("equals");
+
+// 
